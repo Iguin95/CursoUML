93ec635d0007febb750c47c1a9bf48262fc7ef25
Igor Vinnícius <igorquintao22@gmail.com>
2024-11-09 14:10:35 -0300

    Testando o REST

---
+++ src/main/java/com/cursouml/CursoumlApplication.java
@@ -0,0 +1,18 @@
+package com.cursouml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoumlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoumlApplication.class, args);
+	}
+
+	//git init
+	//dar git remote add origin git@github.com:Iguin95/cursoUML.git
+	//git pull origin main
+	//git add .
+	
+}
