68ca44da11170fe61a9c7336a02d83efc57a60ee
Igor Vinnícius <igorquintao22@gmail.com>
2024-11-11 15:22:06 -0300

    Criando a classe de domínio Categoria

--- src/main/java/com/cursouml/CursoumlApplication.java
+++ src/main/java/com/cursouml/CursoumlApplication.java
@@ -8,11 +8,5 @@ public class CursoumlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoumlApplication.class, args);
-	}
-
-	//git init
-	//dar git remote add origin git@github.com:Iguin95/cursoUML.git
-	//git pull origin main
-	//git add .
-	
+	}	
 }
--- src/main/java/com/cursouml/resources/CategoriaResource.java
+++ src/main/java/com/cursouml/resources/CategoriaResource.java
@@ -1,15 +1,29 @@
 package com.cursouml.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cursouml.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String listar() {
-		return "O REST está funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> listar = new ArrayList<>();
+		
+		listar.add(cat1);
+		listar.add(cat2);
+		
+		return listar;
 	}
 }
