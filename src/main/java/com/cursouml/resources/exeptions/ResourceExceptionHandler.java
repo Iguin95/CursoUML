f6c04a19608dba1039e270ed721c91ce37eb77fd
Igor Vinnícius <igorquintao22@gmail.com>
2024-12-09 17:48:02 -0300

    Ajustando o endpoint /categorias/{id} + adicionando tratamento de exceção no método

---
+++ src/main/java/com/cursouml/resources/exeptions/ResourceExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.cursouml.resources.exeptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.cursouml.services.exceptions.ObjectNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e,
+			HttpServletRequest request){
+		
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), Instant.now());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+}
--- src/main/java/com/cursouml/services/CategoriaService.java
+++ src/main/java/com/cursouml/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.cursouml.domain.Categoria;
 import com.cursouml.repositories.CategoriaRepository;
+import com.cursouml.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,8 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id){
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: "
+				+ id + "Tipo: " + Categoria.class.getName()));
 	}
 
 }
